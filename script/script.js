132331845601bac2daa7c92d0cba6e8c7e603943
iknoul <rhmanshamil@gmail.com>
2024-08-15 20:51:47 +0530

    all functional

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
+ // Toggle navigation visibility when the menu icon is clicked
 document.querySelector('#menuBarIcon').addEventListener('click', function() {
     var navAnchors = document.querySelector('.container1');
     var windowWidth = window.innerWidth;
-    //  Toggle naviagation depend on how it will hided according to the screen size
+    // Hiding type based on window width to reduce effects on other containers 
     if(windowWidth>610){
-        // Toggle navigation visibility when the menu icon is clicked
-
             navAnchors.style.setProperty('margin', '0px', 'important');
             navAnchors.style.setProperty('display', 'flex', 'important');
-
     }
     else{
         navAnchors.style.setProperty('margin', '0px', 'important');
@@ -15,9 +13,9 @@ document.querySelector('#menuBarIcon').addEventListener('click', function() {
     }   
 });
 
+// Hide navigation when the back button is clicked
 document.getElementById('menuIconToggle').addEventListener('click', function() {
     var navAnchors = document.querySelector('.container1');
-    // Hide navigation when the back button is clicked
     // Hiding type based on window width to reduce effects on other containers 
     var windowWidth = window.innerWidth;
 
@@ -25,24 +23,74 @@ document.getElementById('menuIconToggle').addEventListener('click', function() {
         navAnchors.style.setProperty('margin-left', '-1000px', 'important');
         navAnchors.style.setProperty('margin-right', '1000px', 'important');
     }
-    else{
-        
+    else{      
         navAnchors.style.setProperty('display', 'none', 'important');
     }
 });
 
 // when side bar menu item class active, it shows sub menu
-document.querySelector('#myBusines').addEventListener('click', function() {
-            this.classList.toggle('active');
-})
-document.querySelector('#finance').addEventListener('click', function() {
-    this.classList.toggle('active');
-})
-document.querySelector('#orgData').addEventListener('click', function() {
-    this.classList.toggle('active');
+ document.querySelectorAll('.menuItem').forEach((item, index) => {
+    
+    var icon = item.querySelector('.menuItem .arrowDown');
+    // if item have sub menu icon, (then only we need to do all other)
+    if(icon){
+        item.addEventListener('click', function() {        
+            if(this.classList.contains('active')){
+                // resetting all values to default if the item already active
+                this.classList.remove('active')
+                this.style.setProperty('background-color', '');
+                icon.style.setProperty('transform', 'rotate(0deg)');
+            }
+            else{
+                // removing the special styles of all other active items
+                document.querySelectorAll('.menuItem').forEach(tab => {
+                    tab.classList.remove('active');            
+                 });
+                document.querySelectorAll('.menuItem .arrowDown').forEach(icon =>{
+                    icon.style.setProperty('transform', 'rotate(0deg)');      
+                })
+                // adding to active class
+                this.classList.add('active');
+                this.style.setProperty('background-color', '#1A213F');
+                icon.style.setProperty('transform', 'rotate(180deg)');
+            }          
+        })
+    }
 })
 
+//when transaction item active it shows the transaction description
+document.querySelectorAll('.transactionDetails').forEach((item, index) => {
 
+    var paymentData = document.querySelectorAll('.paymentData');
+    var icons = document.querySelectorAll('.paymentData img');
+//iterationally adding events to each transaction
+    item.addEventListener('click', function() {
+        if(this.classList.contains('active')){
+            this.classList.remove('active')
+            paymentData[index].style.setProperty('background-color', '#ffffff');
+            icons[index].style.setProperty('transform', 'rotate(180deg)');
+        }
+        // if not already active
+        else{
+            let i = 0
+            //removing all other active transaction properties
+            document.querySelectorAll('.transactionDetails').forEach(trans => {
+                trans.classList.remove('active');
+                paymentData[i].style.setProperty('background-color', '#ffffff');
+                icons[i].style.setProperty('transform', 'rotate(180deg)');
+                i+=1;
+                console.log(paymentData)
+             });
+             // adding to active class
+            this.classList.add('active');
+            icons[index].style.setProperty('transform', 'rotate(0deg)');
+            paymentData[index].style.setProperty('background-color', '#F8F9FC');
+        }
+        
+    })
+})
+
+// shows content on tasks container based on the nav selected
 document.querySelectorAll('.navItem').forEach(item => {
     item.addEventListener('click', function() {
         // Remove active class from all tab items
